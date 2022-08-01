f1d1614460ea6b8f6449be394d968e163e710fd7
m <854284771@qq.com>
2022-08-01 19:39:30 +0800

    feat: init

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -20,26 +20,49 @@ use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
 struct Team {
-    name: String,
+    name: &String,
     goals_scored: u8,
     goals_conceded: u8,
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
-    let mut scores: HashMap<String, Team> = HashMap::new();
+    let mut scores: HashMap<&str, Team> = HashMap::new();
 
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
-        let team_1_name = v[0].to_string();
+        let team_1_name = v[0];
         let team_1_score: u8 = v[2].parse().unwrap();
-        let team_2_name = v[1].to_string();
+        let team_2_name = v[1];
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        let team_1 = scores.entry(team_1_name).or_insert(Team {
+            name: team_1_name.clone(),
+            goals_scored: team_1_score,
+            goals_conceded: team_2_score,
+        });
+        *team_1 = Team {
+            name: team_1_name,
+            goals_scored: team_1.goals_scored + team_1_score,
+            goals_conceded: team_1.goals_conceded + team_2_score,
+        };
+        match scores.get(&team_2_name) {
+            Some(team) => scores.insert(
+                team_2_name,
+                Team {
+                    name: team_2_name,
+                    goals_scored: team.goals_scored + team_2_score,
+                    goals_conceded: team.goals_conceded + team_1_score,
+                },
+            ),
+            None => scores.insert(
+                team_2_name.clone(),
+                Team {
+                    name: team_2_name,
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                },
+            ),
+        };
     }
     scores
 }
